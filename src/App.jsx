6e5eae6f80ad9db08117932af1c49efbd3982a24
Iris408 <ashmagloire45@icloud.com>
2026-05-31 16:58:39 +0100

    feat: add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Header from './components/layout/Header'
+
 function App() {
   return (
     <div className="min-h-screen bg-green-50 flex flex-col">
-      <p className="text-green-800 p-4">Bloom is loading ...</p> 
+
+      <Header />
+
+      <main className="flex-1 flex flex-col items-center justify-start px-4 py-8">  
+        <p className="text-green-700">Welcome to Bloom 🌱</p> 
+      </main>  
+
     </div>
   )
 }
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+    return (
+        <header className="w-full bg-green-800 text-whiite px-6 py-4 flex items-center justify-between">
+
+            {/* Logo - left side */}
+            <div className="flex items-center gap-2">
+                <span className="text-2xl cursor-pointer">🌱</span>
+                <h1 className="text-2xl font-bold tracking-wide cursor-pointer">Bloom</h1>
+            </div>
+
+            {/* Profile avatar - right side */}
+            <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center text-green-800 font-bold cursor-pointer hover:bg-green-100 transition">
+                P
+            </div>
+
+        </header>
+    )
+}
+
+export default Header
