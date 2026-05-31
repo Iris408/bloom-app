5ffcf16b2ba96199daa610eff6aa4e39d0ae43da
Iris408 <ashmagloire45@icloud.com>
2026-05-31 16:43:56 +0100

    feat: initial Bloom project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-green-50 flex flex-col">
+      <p className="text-green-800 p-4">Bloom is loading ...</p> 
+    </div>
+  )
+}
+
+export default App
